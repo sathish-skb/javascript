20f291a1f7bba7de587502689fa1a616de7151fa
Sathish Kumar <31583525+sathishct-2011@users.noreply.github.com>
2021-10-13 01:20:17 +0530

    Create shopping.js

---
+++ shopping.js
@@ -0,0 +1,26 @@
+const shoppingBucket = {
+    userId: soundhar2020,
+    creditCardDiscount: 2000,
+    products: [{
+        productName: "Dell Mosue",
+        productCode: 12357,
+        productPrice: 850,
+        disouctPercentage: 5
+    },
+    {
+        productName: "Dell Monitor",
+        productCode: 12357,
+        productPrice: 18850,
+        disouctPercentage: 5
+    },
+    {
+        productName: "Dell CPU vostro",
+        productCode: 12357,
+        productPrice: 85000,
+        disouctPercentage: 5
+    }]
+}
+
+//total
+
+// 1. If cart total > 30K -> discount
