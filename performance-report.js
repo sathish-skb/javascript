24f6090a8e9c51c6a69b439d418d569b52148c51
Sathish Kumar <31583525+sathishct-2011@users.noreply.github.com>
2021-10-13 19:49:55 +0530

    Create performance-report.js

---
+++ performance-report.js
@@ -0,0 +1,56 @@
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass
+test4    325657    Necho         Policy Change Pass
+test5    325659    Agent  	 ReInstate     Pass
+test6    325645    Customer   	 New Business  Pass
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass    NY
+test4    325657    Necho         Policy Change Pass    NJ
+test5    325659    Agent  	 ReInstate     Pass
+test6    325645    Customer   	 New Business  Pass
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass
+test4    325657    Necho         Policy Change Pass
+test5    325659    Agent  	 ReInstate     Fail
+test6    325645    Customer   	 New Business  Pass
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass
+test4    325657    Necho         Policy Change Pass
+test5    325659    Agent  	 ReInstate     Pass
+test6    325645    Customer   	 New Business  Pass
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass
+test4    325657    Necho         Policy Change Pass
+test5    325659    Agent  	 ReInstate     Pass
+test6    325645    Customer   	 New Business  Pass
+test1    325651    Agent   	 New Business  Pass
+test2    325655    Customer  	 Cencellation  Pass
+test3    325654    SAASE   	 Rewrite       Pass
+test4    325657    Necho         Policy Change Pass
+test5    325659    Agent  	 ReInstate     Pass
+test6    325645    Customer   	 New Business  Pass
+
+
+
+
+
+
+
+{ 
+ searchFilters: [{
+ key: channel
+ value: Agent
+},{
+ key: status
+ value: Fail
+}]
+
+}
+
+
+total scenarios 10
