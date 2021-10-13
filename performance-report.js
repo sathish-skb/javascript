074992c9c928dbdd6106957694d68be96c801ef9
Sathish Kumar <31583525+sathishct-2011@users.noreply.github.com>
2021-10-13 19:55:21 +0530

    Update performance-report.js

--- performance-report.js
+++ performance-report.js
@@ -1,12 +1,12 @@
-test1    325651    Agent   	 New Business  Pass
-test2    325655    Customer  	 Cencellation  Pass
-test3    325654    SAASE   	 Rewrite       Pass
+let performance = [{testid : "test1",   policy:"325651",   channel:" Agent",  	transaction:" New Business", status :"Pass"}]
+test2    325655    Customer  	   Cencellation  Pass
+test3    325654    SAASE   	     Rewrite       Pass
 test4    325657    Necho         Policy Change Pass
-test5    325659    Agent  	 ReInstate     Pass
-test6    325645    Customer   	 New Business  Pass
-test1    325651    Agent   	 New Business  Pass
-test2    325655    Customer  	 Cencellation  Pass
-test3    325654    SAASE   	 Rewrite       Pass    NY
+test5    325659    Agent  	      ReInstate     Pass
+test6    325645    Customer   	  New Business  Pass
+test1    325651    Agent   	     New Business  Pass
+test2    325655    Customer  	   Cencellation  Pass
+test3    325654    SAASE   	     Rewrite       Pass    NY
 test4    325657    Necho         Policy Change Pass    NJ
 test5    325659    Agent  	 ReInstate     Pass
 test6    325645    Customer   	 New Business  Pass
@@ -43,13 +43,11 @@ test6    325645    Customer   	 New Business  Pass
 
 { 
  searchFilters: [{
- key: channel
- value: Agent
-},{
- key: status
- value: Fail
-}]
-
+     key: "channel"
+     value: "Agent" },
+{
+     key: status
+     value: Fail}]
 }
 
 
